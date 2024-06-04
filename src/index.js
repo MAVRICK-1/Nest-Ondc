26f173c7322ca4c7ea363fcd9be99f0a93dc1a61
Jai chouhan <163636237+Jaichouhan458@users.noreply.github.com>
2024-06-04 12:57:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root. render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
