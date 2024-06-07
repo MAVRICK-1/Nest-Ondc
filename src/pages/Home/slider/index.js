714c837f6f4c4840a4542aa99b2cfbb20814a430
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2024-06-08 01:12:54 +0530

    responsive newsletter section in slider

--- src/pages/Home/slider/index.js
+++ src/pages/Home/slider/index.js
@@ -1,17 +1,16 @@
-import React, { useEffect, useContext} from 'react';
+import React, { useContext } from 'react';
 import Slider from "react-slick";
 import './index.css';
 
 import Slide1 from '../../../assets/images/slider-1.png';
 import Slide2 from '../../../assets/images/slider-2.png';
+import Newsletter from '../../../components/newsletter';
+import { MyContext } from '../../../App';
 import Button from '@mui/material/Button';
 
-import Newsletter from '../../../components/newsletter';
 
-import { MyContext } from '../../../App';
 
 const HomeSlider = () => {
-
     const context = useContext(MyContext);
 
     var settings = {
@@ -21,11 +20,9 @@ const HomeSlider = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: true,
-        arrows: context.windowWidth>992 ? true : false,
-        autoplay:true
+        arrows: context.windowWidth > 992 ? true : false,
+        autoplay: true
     };
-
-
 
     return (
         <section className='homeSlider'>
@@ -39,6 +36,7 @@ const HomeSlider = () => {
                                 grocery deals
                             </h2>
                             <p>Sign up for the daily newsletter</p>
+                            <Newsletter /> 
                         </div>
                     </div>
                     <div className="item">
@@ -49,18 +47,13 @@ const HomeSlider = () => {
                                 Big discount
                             </h2>
                             <p>Sign up for the daily newsletter</p>
+                            <Newsletter /> 
                         </div>
                     </div>
                 </Slider>
-
-                {
-                    context.windowWidth>992 && <Newsletter/>
-                }
-                
-
             </div>
         </section>
-    )
+    );
 }
 
 export default HomeSlider;
