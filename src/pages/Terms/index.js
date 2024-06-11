5f196125d6573646b6c977f7504478fb4384e635
Kushagra Singh <88451512+kushagra-singhh@users.noreply.github.com>
2024-06-11 19:16:18 +0530

    Added a TnC Page and Fixed Footer Typo (#93)
    * add tnc page & update app.js
    
    * add TnC page
    
    * remove temp css
    
    * changed formatting of the terms page
    
    * edit TnC page
    
    * fix typo in footer
    
    * remove "Awesome grocery store website template" from footer
    
    * remove TnC button from navbar
    
    * linked TnC page to footer

---
+++ src/pages/Terms/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './style.css';
+
+function Terms() {
+  return (
+    <div className="terms-container">
+      <h1 className="terms-heading">Terms & Conditions</h1>
+      <div className="terms-content">
+      <h2>Terms of Use </h2>
+        <p>This document includes the terms and conditions that you must accept before you access the Nest platform (defined hereinafter). By registering or signing with Nest, or by simply accessing and browsing the Nest platform, you signify your absolute and unconditional consent to all the provisions of this agreement in their entirety.</p>
+        <p>This agreement constitutes a legally binding agreement between Licensee and Nest. You are advised to read this Agreement carefully. If you are not agreeable to any terms and conditions, you should not use this Service and notify the same to Nest.</p>
+        {/* <p>Notwithstanding anything contained in the foregoing, this Agreement will not bind Nest unless you meet the eligibility criteria for entering into this Agreement as set forth in Section 2 of this Agreement.</p> */}
+        <p>The terms “we”, “us”, "Company", "Nest" or “our” shall refer to Nest. The terms “you”, “your”, “seller”, “licensee” shall refer to any individual or entity who accepts this Agreement. Nothing in this Agreement shall be deemed to confer any third-party rights or benefits.</p>
+        <br></br>
+        <h2>Nest Platform</h2>
+        <p>The domain nest-ondc-arzx.vercel.app, including the related subdomains, mobile site, and mobile applications (hereinafter referred to as "Platform" or "Nest") is owned and operated by Nest (hereinafter referred to as “Nest”).</p>
+        <br></br>
+        <h2>Buyer and Seller Transaction and Communication</h2>
+        <p>The Platform facilitates transactions between Buyers and Sellers. Nest is not and cannot be a party to or control any transaction between the Platform's Users. Henceforth:</p>
+        <ul>
+          <li>All commercial/contractual terms are offered by and agreed to between Buyers and Sellers alone. These terms include, but are not limited to, price, shipping costs, payment methods, payment terms, date, period, and mode of delivery, warranties related to products and services, and after-sales services. Nest does not have any control or involvement in the offering or acceptance of such terms between Buyers and Sellers. All discounts and offers (including exchange offers) are provided by the Seller/Brand and not by Nest.</li>
+          <li>When a Buyer places an order with a Seller on the Platform, it is an offer to buy the product(s) from the Buyer to the Seller. This is not construed as the Seller's acceptance of the Buyer's offer. The Seller retains the right to cancel any order at its sole discretion, and the Buyer will be informed via email/SMS. If the Seller cancels an order, any transaction price paid by the Buyer will be refunded. The Seller may also cancel orders that exceed typical individual consumption, based on various factors and at the Seller's sole discretion.</li>
+          <li>Nest makes no representation or warranty regarding the specifics (such as quality, value, salability, etc.) of the products or services proposed to be sold or purchased on the Platform. Nest does not endorse the sale or purchase of any products or services on the Platform and accepts no liability for errors or omissions.</li>
+          <li>Nest is not responsible for any non-performance or breach of any contract between Buyers and Sellers. It cannot guarantee that Buyers and/or Sellers will perform any transaction concluded on the Platform, and it is not required to mediate or resolve disputes.</li>
+          <li>Nest does not make any representation or warranty regarding the item-specifics (such as legal title, creditworthiness, identity, etc.) of its Users. Users are advised to independently verify the bona fides of any particular User they choose to deal with on the Platform.</li>
+          <li>At no point during any transaction does Nest come into possession of the products or services offered by Sellers, nor does it gain title to or have any rights or claims over them. Nest does not hold any right, title, or interest over the products, and it has no obligations or liabilities in respect of the contract entered into between Buyers and Sellers. Nest is not responsible for the unsatisfactory or delayed performance of services or damages or delays due to products that are out of stock, unavailable, or back-ordered.</li>
+          <li>The Platform is a communication platform that Users can utilize to reach a larger base for buying and selling products or services. Nest provides a platform for communication, and it is agreed that the contract for the sale of any products or services is strictly a bipartite contract between the Seller and the Buyer. At no time does Nest hold any right, title, or interest over the products, nor does it have any obligations or liabilities in respect of such contracts. Nest is not responsible for the unsatisfactory or delayed performance of services or damages or delays due to products that are out of stock, unavailable, or back-ordered.</li>
+        </ul>
+        <br></br>
+        <h2>Eligibility</h2>
+        <p>The use of the Nest platform, software, and services are available only to, and may only be used by individuals and companies who can form legally binding contracts under applicable law. Without limiting the foregoing, the Software and Services are not available to children (persons under the age of 18). If You are registering as a business entity, You represent that You have the eligibility to enter into an agreement and the authority to bind the entity to this Agreement. Nest uses many techniques to verify the accuracy of the information you provide when you register on the Nest™ Site. If for any reason, Nest believes such information to be incorrect, it reserves the right to revoke any and all licenses under this Agreement or to refuse to provide the Software license and Services under this Agreement to You.</p>
+        <br></br>
+        <h2>Eligibility and Account Creation</h2>
+        <ul>
+          <li>To be eligible to sell products and/or services through the Platform, the Seller must create an account on the Platform and provide all requested information. The Company reserves the right to reject any account application for any reason, in its sole discretion.</li>
+          <li>The Seller agrees to keep its account information up to date and accurate, and to promptly notify the Company of any changes.</li>
+        </ul>
+        <br></br>
+        <h2>Seller Obligations</h2>
+        <ul>
+          <li>You shall ensure that all the products listed by you on the Platform comply with applicable laws and regulations e.g. Legal Metrology Rules. You shall be solely responsible for the accuracy and completeness of the product listings.</li>
+          <li>You shall ensure that the products listed by you are in stock and available for sale. In case any product listed by you is out of stock, you shall remove the listing from the Platform promptly.</li>
+          <li>You shall be responsible for shipping the products to the customers in accordance with the delivery timelines specified on the Platform. You shall ensure that the products are packaged in a safe and secure manner and delivered to the customers without any damage.</li>
+          <li>You shall be solely responsible for all taxes, duties, and other levies applicable to the sale of your products. You shall also be responsible for obtaining all necessary licenses and permits for selling your products on the Platform.</li>
+          <li>You shall ensure that you have all necessary rights, licenses, and permissions to list and sell the products on the Platform.</li>
+          <li>You shall maintain accurate and complete records of all transactions and sales made through the Platform. You shall provide such records to Nest.in upon request.</li>
+          <li>You shall not engage in any fraudulent or deceptive practices while using the Platform. You shall not misrepresent the quality, nature, or authenticity of the products listed by you on the Platform.</li>
+        </ul>
+        <br></br>
+        <h2>Obligations and Compliance as ONDC Network Participant</h2>
+        <ul>
+          <li>Nest, as a designated participant within the ONDC network, bears a solemn duty to adhere to the prevailing ONDC regulations, rules, and directives.</li>
+          <li>The seller, unequivocally consents to comply with the regulations and directives issued by ONDC from time to time. These directives could be related to catalog quality, order fulfillment, order delivery, order cancellation, or customer grievances.</li>
+          <li>Seller also authorizes Nest to furnish ONDC and its associated entities with access to information and content, subject to the stipulated guidelines.</li>
+          <li>In its capacity as an ONDC Network Participant, Nest shall undertake the tasks of adjusting, altering, and disseminating data in accordance with the standardized format as prescribed by the ONDC protocol.</li>
+        </ul>
+        <br></br>
+        <h2>Intellectual Property</h2>
+        <ul>
+          <li>The Seller shall ensure that all content, including product descriptions, images, and other materials provided by the Seller, do not infringe on any third party’s intellectual property rights.</li>
+          <li>The Seller grants the Company a non-exclusive, royalty-free, perpetual, and worldwide license to use, reproduce, modify, and display the content provided by the Seller for the purpose of operating the Platform and promoting the Seller’s products.</li>
+        </ul>
+        <br></br>
+        {/*-----------------------Optional Content for future use----------------------------*/}
+        {/* <h2>Termination</h2>
+        <ul>
+          <li>The Company reserves the right to terminate or suspend the Seller’s account at any time, without notice, for any reason, including but not limited to violation of these Terms and Conditions or applicable laws and regulations.</li>
+          <li>Upon termination, the Seller shall immediately cease all use of the Platform and remove all product listings from the Platform.</li>
+          <li>The Seller’s obligations under these Terms and Conditions shall survive the termination of the Seller’s account.</li>
+        </ul>
+        <br></br>
+        <h2>Limitation of Liability</h2>
+        <ul>
+          <li>The Company shall not be liable for any indirect, incidental, consequential, or punitive damages arising out of or in connection with the Seller’s use of the Platform or the sale of products through the Platform.</li>
+          <li>The Company’s total liability to the Seller for any claim arising out of or in connection with these Terms and Conditions shall not exceed the amount of fees paid by the Seller to the Company in the six months preceding the date of the claim.</li>
+        </ul>
+        <br></br>
+        <h2>Governing Law and Jurisdiction</h2>
+        <ul>
+          <li>These Terms and Conditions shall be governed by and construed in accordance with the laws of India.</li>
+          <li>Any disputes arising out of or in connection with these Terms and Conditions shall be subject to the exclusive jurisdiction of the courts of Gurugram, Haryana.</li>
+        </ul> */}
+      </div>
+    </div>
+  );
+}
+
+export default Terms;
